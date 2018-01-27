6962c393ce99ccb0abece9496ee9855d0599497c
Vijay Vankhede <vijay@active.ai>
2018-01-27 18:43:31 +0530

    feat: mock api request

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.deb';
+import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
@@ -16,7 +16,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function (req, res) {
+    res.json([
+        {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+        {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+        {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    ]);
 });
 
 app.listen(port, function (err) {
